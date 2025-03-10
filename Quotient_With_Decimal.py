aaaadcde015f358c2241596f67cafc11c22dfae9
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:51:25 +0800

    change variable name

--- Quotient_With_Decimal.py
+++ Quotient_With_Decimal.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
 print("Quotient")
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print("The quotient is:", num1 / num2)
+print("The quotient is:", first_number / second_number)
