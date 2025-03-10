614fea402cf2f805ff78fe90078fcb68439a25a0
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 22:46:32 +0800

    getting the input

---
+++ print_the_bigger_number.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 2 numbers. Print the bigger number.
+
+print("The Bigger Number")
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
