a2253484fde6c610828d887205acca8c74d24f22
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:16:45 +0800

    rename

---
+++ Print_The_Bigger_Number.py
@@ -0,0 +1,10 @@
+#Create a program that ask user to input 2 numbers. Print the bigger number.
+
+print("print the bigger number")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    print("The bigger number is: ", num1)
+else:
+    print("The bigger number is: ", num2)
