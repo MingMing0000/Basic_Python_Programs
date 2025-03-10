a4f8f04b47f0c3eb25500386d8308ee67ce9a168
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 22:41:41 +0800

    creating the for loop and putting the condition

---
+++ print_numbers_1_to_100_except_ends_with_zero.py
@@ -0,0 +1,6 @@
+#Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+print("Numbers from 0 to 100 except numbers ending in zero:")
+for i in range(101):
+    if i % 10 != 0:
+        print(i)
