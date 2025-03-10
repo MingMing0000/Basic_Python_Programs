3ffa4458cef12673de9a6d62b2030e494b8cd6a7
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 20:40:51 +0800

    Getting Input

---
+++ Sum_Of_Two_Numbers.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+print("Sum of Two Numbers")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
