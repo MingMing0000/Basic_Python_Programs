dd16f9248be075e4a3bac37076a405bdc583b579
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:54:04 +0800

    change variable name

--- Sum_Of_Two_Numbers.py
+++ Sum_Of_Two_Numbers.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 print("Sum of Two Numbers")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
-print("The sum is:", num1 + num2)
+print("The sum is:", first_number + second_number)
