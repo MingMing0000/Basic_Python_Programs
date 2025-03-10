5603fc95f7e15496be56c5036516caf1a4db7c13
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 20:50:36 +0800

    Getting input

---
+++ Quotient_With_Decimal.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+print("Quotient")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
