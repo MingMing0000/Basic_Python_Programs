6de71142807ab9a2c487064838ad8f95e388fd63
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 22:25:36 +0800

    add to list and get the sum

--- Sum_Of_All_Numbers.py
+++ Sum_Of_All_Numbers.py
@@ -1,10 +1,12 @@
 #Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 print("Sum of all numbers")
-
+number_list = []
 for i in range(1, 11):
     number = (input(f"({i}) Enter a number: "))
     number = int(number)
-    number += number
+    number_list.append(number)
     
-print("The sum is:", number)
+result = sum(number_list)
+    
+print("The sum is:", result)
