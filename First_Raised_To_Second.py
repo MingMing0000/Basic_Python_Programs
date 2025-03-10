5f8c63d99ed4311e5a790736ad8439d2e0fe60a7
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:33:07 +0800

    change variable name

--- First_Raised_To_Second.py
+++ First_Raised_To_Second.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 print("Exponent")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
-print("The result is:", num1 ** num2)
+print("The result is:", first_number ** second_number)
