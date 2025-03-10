b6bc5bb5313bba3de4ab18f504bedad265a1e6a8
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 20:39:17 +0800

    Test if equal

--- Print_Equal_If_Same.py
+++ Print_Equal_If_Same.py
@@ -1,4 +1,10 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 print("Equal if Same")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+if num1 == num2:
+    print("Equal")
+else:
+    exit()
