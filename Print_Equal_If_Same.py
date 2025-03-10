e6d1057c83b8c3831b43ccd69f4e6b34bb6e9873
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:34:19 +0800

    change variable name

--- Print_Equal_If_Same.py
+++ Print_Equal_If_Same.py
@@ -1,10 +1,10 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 print("Equal if Same")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
-if num1 == num2:
+if first_number == second_number:
     print("Equal")
 else:
     exit()
