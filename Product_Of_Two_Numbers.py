bf00801271f1b8110400dacb2d22a609aa691ef2
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 21:50:27 +0800

    change variable name according to coding standard

--- Product_Of_Two_Numbers.py
+++ Product_Of_Two_Numbers.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 print("Product of Two Numbers")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
-print("The product is:", num1 * num2)
+print("The product is:", first_number * second_number)
