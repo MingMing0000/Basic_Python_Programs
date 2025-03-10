f2ec9a36a6a4e70798b017c1b98bfd58ae20295e
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 22:47:02 +0800

    show the bigger number

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -4,3 +4,7 @@ print("The Bigger Number")
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
+if first_number > second_number:
+    print(f"The bigger number is {first_number}")
+else:
+    print(f"The bigger number is {second_number}")
