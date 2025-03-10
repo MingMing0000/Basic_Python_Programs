131698fc887e1d1f770f3ac1a1b20543ce5e9a9b
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 20:57:06 +0800

    Getting Input

---
+++ First_Raised_To_Second.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+print("Exponent")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
