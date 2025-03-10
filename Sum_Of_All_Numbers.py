f0e362ef109a97bc4478de8f356eeb912f3e07a7
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-10 22:04:32 +0800

    getting the input

---
+++ Sum_Of_All_Numbers.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+print("Sum of all numbers")
+
+for i in range(1, 11):
+    number = (input(f"({i}) Enter a number: "))
+    number = int(number)
+    number += number
+    
