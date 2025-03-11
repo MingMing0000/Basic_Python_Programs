75de444989648b29c37ac15c577e003636a93a9d
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 09:17:38 +0800

    create while loop and put the conditon

---
+++ odd_numbers_from_0_to_100.py
@@ -0,0 +1,8 @@
+#Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+print("all the odd numbers starting from 0 to 100 using while loop")
+
+while number <= 100:
+    if number % 2 != 0:
+        print(number)
+    number += 1
