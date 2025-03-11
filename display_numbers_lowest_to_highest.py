5a05c500ef8543e74b8d79a97e2848998b1ce4e1
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 11:27:47 +0800

    get user input and add to list

---
+++ display_numbers_lowest_to_highest.py
@@ -0,0 +1,11 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+number_list = []
+
+#Ask user to input a number until invalid
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        number_list.append(number)
+    except ValueError:
+        break
