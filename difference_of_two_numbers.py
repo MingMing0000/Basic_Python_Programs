7fde4b65069d1980523d04ff7a74ad95eb45b0d5
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:16:36 +0800

    getting the input

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+print("The difference of two numbers")
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
