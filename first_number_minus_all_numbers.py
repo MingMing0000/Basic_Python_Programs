cc7b394a6eeb4f90475ca54a113a6367d4e44e68
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:58:17 +0800

    change to for loop and add to list

--- first_number_minus_all_numbers.py
+++ first_number_minus_all_numbers.py
@@ -1,6 +1,7 @@
 #Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 print("The first number minus all of the remaining numbers")
-
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+number_list = []
+for i in range(10):
+    number = int(input("Enter a number: "))
+    number_list.append(number)
