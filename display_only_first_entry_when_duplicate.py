5dc0c677dfdc25068464d14bea19a7b3d09dfc98
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 10:32:24 +0800

    print title and vreate variable

---
+++ display_only_first_entry_when_duplicate.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+print("Enter 10 numbers")
+
+numbers_list = set()
+
