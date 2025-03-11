ebad3cdda885e2ba8d3ba8963c0d5aa550aaddf2
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:09:07 +0800

    showing the smaller number

--- print_the_smaller_number.py
+++ print_the_smaller_number.py
@@ -4,3 +4,8 @@ print("The smaller number")
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+if first_number < second_number:
+    print(f"the smaller number is {first_number}")
+else:
+    print(f"the smaller number is {second_number}")
