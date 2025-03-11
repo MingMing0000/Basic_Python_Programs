896b2456710d07449f59406a79029c98fa8d0a93
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 11:24:53 +0800

    Getting input from user and add to list

---
+++ display_the_lowest_number.py
@@ -0,0 +1,11 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+number_list = []
+
+#Ask user to input a number until invalid
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        number_list.append(number)
+    except ValueError:
+        break
