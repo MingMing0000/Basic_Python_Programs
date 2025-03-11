72792def389f4415acb71f66db6395d53d010fc1
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 09:14:01 +0800

    put condition and count the even numbers

--- how_many_are_even.py
+++ how_many_are_even.py
@@ -3,5 +3,8 @@
 print("how many are even numbers")
 print("Enter 10 numbers: ")
 
+even_numbers_count = 0
 for i in range(10):
-    num = int(input("Enter a number: "))
+    number = int(input("Enter a number: "))
+    if number % 2 == 0:
+        even_numbers_count += 1
