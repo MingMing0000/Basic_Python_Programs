a837b0bc6481c6c2afd52554e3836a466fc9f62e
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 10:33:46 +0800

    create for loop and

--- display_only_first_entry_when_duplicate.py
+++ display_only_first_entry_when_duplicate.py
@@ -4,3 +4,8 @@ print("Enter 10 numbers")
 
 numbers_list = set()
 
+for i in range(10):
+    input_number = int(input("Enter number: "))
+    numbers_list.add(input_number)
+
+print(f"The Numbers are: {numbers_list}")
