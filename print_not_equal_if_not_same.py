46fd171f1948a19b5ca3135dd8da42439bfe6a3b
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:10:59 +0800

    getting input

---
+++ print_not_equal_if_not_same.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+print("Not Equal if not the same")
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
