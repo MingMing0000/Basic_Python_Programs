3cc1734b68534f85ec6529c251e32f7d92c9c63c
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:18:07 +0800

    show the difference

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -4,3 +4,6 @@ print("The difference of two numbers")
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+difference = first_number - second_number
+print(f"The difference of the two numbers is {difference}")
