d6b85409945cdca18a8e754db88f9628cebc0755
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:06:16 +0800

    getting input

---
+++ print_the_smaller_number.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 2 numbers. Print the smaller number.
+
+print("The smaller number")
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
