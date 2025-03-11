91fbd77eb90857ca02918a12e7fbf0ee19aa2327
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:30:50 +0800

    getting input

---
+++ first_number_minus_all_numbers.py
@@ -0,0 +1,6 @@
+#Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+print("The first number minus all of the remaining numbers")
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
