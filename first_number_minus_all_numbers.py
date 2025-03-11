45a4216dc9776875775ef7817029dd03a87f5f0b
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:58:44 +0800

    calculate and show the difference

--- first_number_minus_all_numbers.py
+++ first_number_minus_all_numbers.py
@@ -5,3 +5,6 @@ number_list = []
 for i in range(10):
     number = int(input("Enter a number: "))
     number_list.append(number)
+
+difference = number_list[0] - sum(number_list[1:])
+print(f"The result of the first number minus all of the remaining numbers is {difference}")
