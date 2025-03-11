3ff8f66ced0bc6d1740cc5938d7832b837305ac2
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 08:22:02 +0800

    show the quotient

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -4,3 +4,6 @@ print("The quotient of two numbers without the decimal point")
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+quotient = first_number // second_number
+print(f"The quotient of the two numbers is {quotient}")
