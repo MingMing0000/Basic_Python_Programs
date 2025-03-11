5aa7c9d9e13d7136ddb928e068339abc8c131c38
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 09:32:56 +0800

    add a title

--- all_numbers_between_two_numbers.py
+++ all_numbers_between_two_numbers.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 print('Numbers between two numbers')
-print("Enter two numbers:")
+print("Enter two numbers to get all the numbers between them")
 
 First_number = int(input("Enter first number: "))
 Second_number = int(input("Enter second number: "))
