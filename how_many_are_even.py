41a509554384af78c263a6c52a2a0a64b31f13c5
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-11 09:10:59 +0800

    getting input

---
+++ how_many_are_even.py
@@ -0,0 +1,7 @@
+#Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+print("how many are even numbers")
+print("Enter 10 numbers: ")
+
+for i in range(10):
+    num = int(input("Enter a number: "))
