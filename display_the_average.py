060ddb9872bf964afae825e0dbfd6d29b1f9facd
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-12 20:20:57 +0800

    create list and get user input

---
+++ display_the_average.py
@@ -0,0 +1,13 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+print("Display the average")
+#Create a list to store the numbers
+numbers_list = []
+
+#Ask user to input a number
+while True:
+    try:
+        number_input = int(input("Enter a number: "))
+        numbers_list.append(number_input)
+    except:
+        break
